5be94e97522aa62b1b3e70195ac54d0ada921568
hp <hp@LAPTOP-N5SF7R7D>
2020-12-23 19:40:02 +0530

    Version 2.2

---
+++ CGLVersion_2.1/src/Cell.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+/**
+ * @author L.Sri Rama Karthikeya
+ * Class in which a cell initial state is dead
+ */
+public class Cell {
+	private boolean state=false;
+     public boolean getState(){
+	       return state;
+	    }
+	    public void changeState() {
+	        state = true;
+	    }
+	}
---
+++ CGLVersion_2.1/src/Conways.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+/**
+ * @author L.Sri Rama Karthikeya
+ *
+ */
+public class Conways {
+	public boolean getState() {
+		return false;
+	}
+    
+
+}
